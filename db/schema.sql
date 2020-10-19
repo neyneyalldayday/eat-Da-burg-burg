f606dd223eb82417277a6f6b85bffa1b7129571a
NeyNey <drfrankenstein123@gmail.com>
2020-10-19 16:53:16 -0500

    creating connection

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers(
-id INT NOT NULL AUTO_INCREMENT,
+id INT NOT NULL  AUTO_INCREMENT,
  burger_name VARCHAR (100) NOT NULL,
  devoured BOOLEAN ,
  PRIMARY KEY (id)
