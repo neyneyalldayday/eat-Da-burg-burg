1134bea84239dfeabb3554e98031dfa3e92b661d
NeyNey <drfrankenstein123@gmail.com>
2020-10-19 16:34:51 -0500

    adding databases

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+USE  burger_db;
+
+
+INSERT INTO burgers (
+burger_name
+)
+VALUES (
+"fish taco burger"
+);
+
+INSERT INTO burgers (
+burger_name
+)
+VALUES (
+"spagetti icecream burger"
+);
+
+INSERT INTO burgers (
+burger_name
+)
+VALUES (
+"chilli choking pepper belly burger"
+);
+
