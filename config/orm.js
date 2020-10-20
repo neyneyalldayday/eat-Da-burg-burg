f2baf586759e09d830ec014698184d32ee565a10
NeyNey <drfrankenstein123@gmail.com>
2020-10-20 11:03:59 -0500

    updating orm

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,26 @@
 var connection = require("./connection.js");
 
-var orm =
-selectAll();
-insertOne();
-updateOne();
+var orm = {
+    selectAll: function(tableInput, consb) {
+        let querryStr = "SELECT * FROM" + tableInput + ";";
+        connection.query(queryStr, function (err, res) {
+            if (err) throw err;
+            consb(res)
+        })
+    },
+    insertOne: function(newBurg, consb) {
+        let querryStr = "INSERT INTO burgers (burger_name) VALUES ('" + newBurg + "');";
+        connection.query(queryStr,function(err, res) {
+            if(err) throw err;
+            consb(res);
+        })
+    },
+    updateOne: function(burgId, consb) {
+        let queryStr = "UPDATE burgers SET devoured = true WHERE ID = " + burgId + ";";
+        connection.query(queryStr, function(err, res) {
+            if (err) throw err;
+        })
+    }
+}
 
 module.exports = orm;
