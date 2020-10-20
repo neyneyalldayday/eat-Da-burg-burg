a761d38f149042cde2c8958a8abda676992e0b36
NeyNey <drfrankenstein123@gmail.com>
2020-10-20 10:39:57 -0500

    burger.js model

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,16 @@
 var orm = require("./config/orm");
 
+var burg = {
+    selectAll: function(consb) {
+        orm.selectAll("burgers", function(res) {
+            consb(res);
+        })
+    },
+    insertOne: function(newBurg, consb) {
+        orm.insertOne(newBurg, function(res) {
+            consb(res);
+        })
+    }
+}
+
 module.exports =  burger;
