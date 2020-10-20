6bb8cf976f212fddfa79bfbfcc10b3d1991ad1d1
NeyNey <drfrankenstein123@gmail.com>
2020-10-20 10:50:27 -0500

    updating controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,5 +1,26 @@
 var express = require("express");
 var burger = require("./models/burger");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        res.render("index", { burgers: data});
+    })
+})
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([req.body.name], function(result) {
+        res.json({ id: result.insertId });
+    })
+})
+router.put("/api/burgers/:id", function(req, res) {
+    let burgId = req.params.id;
+    burger.updateOne(burgId, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end()
+    })
+})
 
 
 
